5e3be891d26eef14f92165791f9cd6daea8e2225
Youssef Shoaib <yhshoaib@students.wcpss.net>
2019-09-12 07:03:22 -0400

    Cleanup code

--- PasswordGeneratorForms/Form1.Designer.cs
+++ PasswordGeneratorForms/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(46, 12);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 47);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(0, 13);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(164, 69);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblPassword;
     }
 }
 
--- PasswordGeneratorForms/Form1.cs
+++ PasswordGeneratorForms/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PasswordGeneratorForms
@@ -15,6 +10,69 @@ namespace PasswordGeneratorForms
         public Form1()
         {
             InitializeComponent();
+        }
+        private static Random rndRandomGenerator = new Random();
+
+        //An enum of the required character types
+        private enum CharType
+        {
+            //Signifies a number from 1 to 9 inclusive
+            Number,
+            //Signifies a letter from a-z inclusive
+            Lowercase,
+            //Signifies a letter from A-Z inclusive
+            Uppercase,
+            //Signifies a letter from the SPECIAL_CHARS string
+            Special
+        }
+
+        //The required length of the password
+        private const int PASSWORD_LENGTH = 6;
+
+        //The required length of the password
+        private const int PASSWORD_REQUIREMENTS_LENGTH = 4;
+
+        //All the special characters allowed in the password
+        private const string SPECIAL_CHARS = "!@#$%";
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                string strPassword = "";
+                //Generate all 4 types
+                for (int j = 0; j < PASSWORD_REQUIREMENTS_LENGTH; j++)
+                    strPassword += GenerateRandomCharByType((CharType)j);
+
+                //Generate 2 more random letters
+                for (int j = 0; j < PASSWORD_LENGTH - PASSWORD_REQUIREMENTS_LENGTH; j++)
+                {
+                    strPassword += GenerateRandomCharByType((CharType)rndRandomGenerator.Next(0, 4));
+                }
+
+                //Randomize the order of all the alphanumeric characters in the password
+                strPassword = string.Join("", strPassword.OrderBy(x => rndRandomGenerator.Next(strPassword.Length)));
+                //Show the password to the user
+                lblPassword.Text = "Your pasword is: " + strPassword;
+            }
+        }
+
+        //This function takes in the type of the character taht you want and returns a random character from that pool of characters
+        private static char GenerateRandomCharByType(CharType type)
+        {
+            switch (type)
+            {
+                case CharType.Number:
+                    return rndRandomGenerator.Next(1, 10).ToString()[0];
+                case CharType.Lowercase:
+                    return (char)('a' + rndRandomGenerator.Next(0, 26));
+                case CharType.Uppercase:
+                    return (char)('A' + rndRandomGenerator.Next(0, 26));
+                case CharType.Special:
+                    return SPECIAL_CHARS[rndRandomGenerator.Next(SPECIAL_CHARS.Length)];
+                default:
+                    //This should never happen, but just in case.
+                    return ' ';
+            }
         }
     }
 }
